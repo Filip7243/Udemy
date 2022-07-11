eda52501f8b30903d3ab4840c37d7be83bda09dc
Filip7243 <filip7243@gmail.com>
2022-07-11 19:13:26 +0200

    done

--- src/Main.java
+++ src/Main.java
@@ -15,14 +15,75 @@ public class Main {
             ByteBuffer buffer = ByteBuffer.allocate(100);
             byte[] msg = "Hello World!".getBytes();
             buffer.put(msg);
+            long int1pos = msg.length;
             buffer.putInt(213);
+            long int2pos = int1pos + Integer.BYTES;
             buffer.putInt(-21321);
             byte[] msg2 = "Nice to meet you!".getBytes();
             buffer.put(msg2);
+            long int3pos = int2pos + Integer.BYTES + msg2.length;
+            buffer.putInt(1000);
             buffer.flip();
             binChanel.write(buffer);
 
-            ByteBuffer readBuffer = ByteBuffer.allocate(100);
+            ByteBuffer readBuffer = ByteBuffer.allocate(Integer.BYTES);
+            raChanel.position(int3pos);
+            raChanel.read(readBuffer); // at this time in buffer is only an int3
+            readBuffer.flip();
+            System.out.println("int3 = " + readBuffer.getInt());
+
+            readBuffer.flip();
+            raChanel.position(int2pos);
+            raChanel.read(readBuffer);
+            readBuffer.flip();
+            System.out.println("int2 = " + readBuffer.getInt());
+
+            readBuffer.flip();
+            raChanel.position(int1pos);
+            raChanel.read(readBuffer);
+            readBuffer.flip();
+            System.out.println("int1 = " + readBuffer.getInt());
+
+            byte[] outputString = "Hello World!".getBytes();
+            long str1pos = 0;
+            long newInt1pos = outputString.length;
+            long newInt2pos = newInt1pos + Integer.BYTES;
+            byte[] outputString2 = "Nice to meet you!".getBytes();
+            long str2pos = newInt2pos + Integer.BYTES;
+            long newInt3pos = str2pos + outputString2.length;
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+
+            intBuffer.putInt(245);
+            intBuffer.flip();
+            binChanel.position(newInt1pos);
+            binChanel.write(intBuffer);
+
+
+            intBuffer.flip();
+            intBuffer.putInt(-321321);
+            intBuffer.flip();
+            binChanel.position(newInt2pos);
+            binChanel.write(intBuffer);
+
+            intBuffer.flip();
+            intBuffer.putInt(1000);
+            intBuffer.flip();
+            binChanel.position(newInt3pos);
+            binChanel.write(intBuffer);
+
+            binChanel.position(str1pos);
+            binChanel.write(ByteBuffer.wrap(outputString));
+
+            binChanel.position(str2pos);
+            binChanel.write(ByteBuffer.wrap(outputString2));
+
+
+
+
+
+
+            /*ByteBuffer readBuffer = ByteBuffer.allocate(100);
             raChanel.read(readBuffer);
             readBuffer.flip();
             byte[] inputString = new byte[msg.length];
@@ -32,7 +93,7 @@ public class Main {
             System.out.println("int2 = " + readBuffer.getInt());
             byte[] inputString2 = new byte[msg2.length];
             readBuffer.get(inputString2);
-            System.out.println("inputString2 = " + new String(inputString2));
+            System.out.println("inputString2 = " + new String(inputString2));*/
 
 //            byte[] msg = "Hello World!".getBytes();
 //            ByteBuffer buffer = ByteBuffer.allocate(msg.length); //when we using wrap we dont have to put anything to buffer because it is doing for us and flips buffer to position 0 after all,
